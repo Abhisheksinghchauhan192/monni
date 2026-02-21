a22d1e8fce15815c75f9f0561d8895bd0e9cac7a
Abhishek Singh Chauhan <chauhanavi667@gmail.com>
2026-02-21 21:43:32 +0530

    fix:now must add special chars in password while registering

--- server/src/validators/auth/auth.schema.js
+++ server/src/validators/auth/auth.schema.js
@@ -4,20 +4,27 @@ export const registerSchema = z.object({
   name: z.string().min(2).trim(),
 
   email: z
-    .string().trim()
+    .string()
+    .trim()
     .email()
     .transform((email) => email.toLowerCase()),
 
   password: z
-    .string().trim()
+    .string()
+    .trim()
     .min(6, "Password must be at least 6 characters")
     .regex(/[A-Za-z]/, "Password must contain at least one letter")
-    .regex(/[0-9]/, "Password must contain at least one number"),
+    .regex(/[0-9]/, "Password must contain at least one number")
+    .regex(
+      /[^A-Za-z0-9]/,
+      "Password must contain at least one special character",
+    ),
 });
 
 export const loginSchema = z.object({
   email: z
-    .string().trim()
+    .string()
+    .trim()
     .email()
     .transform((email) => email.toLowerCase()),
 
