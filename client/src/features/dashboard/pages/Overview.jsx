acf921ca47554cf589bd31471002a50bf237b6ce
Abhishek Singh Chauhan <chauhanavi667@gmail.com>
2026-03-03 21:42:01 +0530

    UI:improvement in trendchart

--- client/src/features/dashboard/pages/Overview.jsx
+++ client/src/features/dashboard/pages/Overview.jsx
@@ -1,12 +1,13 @@
 import TopSection from "../sections/TopSection";
 import AnalyticsSection from "../sections/AnalyticalSection";
-import AddExpenseButton from "../components/AddExpenseButton";
+import Footer from "../../../components/Footer";
 
 export default function Overview() {
   return (
     <div className="space-y-10">
       <TopSection />
       <AnalyticsSection />
+      <Footer/>
     </div>
   );
 }
