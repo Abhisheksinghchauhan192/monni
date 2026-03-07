4d2b1dc405f2592aa24b30079eac80f08190d793
Abhishek Singh Chauhan <chauhanavi667@gmail.com>
2026-03-07 09:27:16 +0530

    fix(server):fixed cors error for client (#15)

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: ["http://localhost:5173","http://10.127.127.50:5173"],
+    origin: ["http://localhost:5173","https://monni-one.vercel.app/"],
     methods: ["PUT", "GET", "POST", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
